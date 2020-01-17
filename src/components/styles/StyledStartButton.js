49c74c03ff2a2525fbc21db65d262f2f42f750bc
Felix He <felixhe.ski@outlook.com>
2020-01-17 01:47:22 -0500

    Added React Hooks

---
+++ src/components/styles/StyledStartButton.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledStartButton = styled.button`
+    border: none;
+    box-sizing: border-box;
+    margin: 0 0 20px 0;
+    outline: none;
+    padding: 20px;
+    cursor: pointer;
+    width: 100%;
+    border-radius: 20px;
+    font-family: Pixel, Arial, Helvetica, sans-serif;
+    color: white;
+    background: #333;
+    font-size: item;
+    min-height: 30px;  
+`
